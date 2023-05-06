fbaf816b8effb1c9e70bed14504b3fdad5b904d2
ch4ntel <88483937+ch4ntel@users.noreply.github.com>
2023-05-06 12:03:38 +0800

    authentication (backend): update
    authentication (backend): update

--- Python/backend/controllers/auth.py
+++ Python/backend/controllers/auth.py
@@ -1,40 +1,55 @@
-from models.model import BankModel, db
+from models.model import Claims, db
 from flask import request, jsonify
 from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
 
 # Making sure that the request parsed fits the below guidelines
 put_args = reqparse.RequestParser()
 # currently no good error message, only 500
-put_args.add_argument("Name", type=str, help = "Customer name is required", required = True) 
-put_args.add_argument("ID", type=str, help = "Customer acocunt ID", required = True)
-put_args.add_argument("Balance", type=str, help = "Customer balance", required = True)
-put_args.add_argument("Bank", type=str, help = "POSB or DBS")
-
+#put_args.add_argument("Name", type=str, help = "Customer name is required", required = True) 
+#put_args.add_argument("ID", type=str, help = "Customer acocunt ID", required = True)
+#put_args.add_argument("Balance", type=str, help = "Customer balance", required = True)
+#put_args.add_argument("Bank", type=str, help = "POSB or DBS")
+put_args.add_argument("ClaimID", type=int, help = "ClaimID is required", required = True) 
+put_args.add_argument("ProjectID", type=int, help = "ProjectID is required", required = True) 
+put_args.add_argument("CurrencyID", type=str, help = "Currency ID is required", required = True)
+put_args.add_argument("EmployeeID", type=int, help = "Employee ID is required", required = True)
+put_args.add_argument("ExpenseDate", type=int, help = "Expense Date is required", required = True)
+put_args.add_argument("Amount", type=float, help = "Amount is required", required = True)
+put_args.add_argument("Purpose", type=str, help = "Purpose is required", required = True)
+put_args.add_argument("DepartmentCode", type=int, help = "Key in Department Code if available")
 
 update_args = reqparse.RequestParser()
-update_args.add_argument("Name", type=str, help = "Customer name is required")
-update_args.add_argument("ID", type=str, help = "Customer acocunt ID")
-update_args.add_argument("Balance", type=str, help = "Customer balance")
-update_args.add_argument("Bank", type=str, help = "POSB or DBS")
+#update_args.add_argument("Name", type=str, help = "Customer name is required")
+#update_args.add_argument("ID", type=str, help = "Customer acocunt ID")
+#update_args.add_argument("Balance", type=str, help = "Customer balance")
+#update_args.add_argument("Bank", type=str, help = "POSB or DBS")
+update_args.add_argument("ClaimID", type=int, help = "ClaimID is required", required = True) 
+update_args.add_argument("ProjectID", type=int, help = "ProjectID is required", required = True) 
+update_args.add_argument("CurrencyID", type=str, help = "Currency ID is required", required = True)
+update_args.add_argument("EmployeeID", type=int, help = "Employee ID is required", required = True)
+update_args.add_argument("ExpenseDate", type=int, help = "Expense Date is required", required = True)
+update_args.add_argument("Amount", type=float, help = "Amount is required", required = True)
+update_args.add_argument("Purpose", type=str, help = "Purpose is required", required = True)
+update_args.add_argument("DepartmentCode", type=int, help = "Key in Department Code if available")
 
-resource_fields = {
+'''resource_fields = {
     'ID' : fields.Integer,
     "Name" : fields.String,
     "Balance" : fields.Integer,
     "Bank" : fields.String
-}
+}'''
 
 # @marshal_with(resource_fields)
 def get(id):
         # Store query results
-        result = BankModel.query.filter_by(ID = id)
+        result = Claims.query.filter_by(ClaimID = id)
 
         if not result:
-            abort(409, message = "Client's account could not be found")
+            abort(409, message = "Client's claim could not be found")
         return jsonify(
              {
                   "code": 200,
-                  "data": [row.json() for row in result]
+                  "body": [row.json() for row in result]
              }
         )
 
@@ -42,13 +57,14 @@ def get(id):
 def put(id):
     # Get arguments from put_args as defined above, where missing fields will have error
     args = put_args.parse_args()
-    Client = BankModel(ID = args['ID'], Name = args['Name'],
-                        Balance = args['Balance'], Bank = args['Bank'])
+    Client = Claims(ClaimID = args['ClaimID'], ProjectID = args['ProjectID'], CurrencyID = args['CurrencyID'],
+                        EmployeeID = args['EmployeeID'], ExpenseDate = args['ExpenseDate'],
+                        Amount = args['Amount'], Purpose = args['Purpose'], DepartmentCode = args['DepartmentCode'])
     
     # Check if field already exists
-    result = BankModel.query.filter_by(ID = args['ID']).first()
+    result = Claims.query.filter_by(ClaimID = args['ClaimID']).first()
     if result:
-        abort(409, message = "Bank account already exists")
+        abort(409, message = "Claim already exists")
 
     # Add object to staging
     db.session.add(Client)
@@ -59,18 +75,30 @@ def put(id):
 
 def patch(id):
         args = update_args.parse_args()
-        result = BankModel.query.filter_by(ID = id).first()
+        result = Claims.query.filter_by(ClaimID = id).first()
         if not result:
-            abort(404, message = "Cannot update an account that does not exist")
+            abort(404, message = "Cannot update a claim that does not exist")
 
-        if args['Name']:
-            result.name = args["Name"]
+        if args['ProjectID']:
+            result.name = args["ProjectID"]
         
-        if args['Bank']:
-            result.bank = args["Bank"]
+        if args['CurrencyID']:
+            result.bank = args["CurrencyID"]
         
-        if args['Balance']:
-            result.balance = args["Balance"]
+        if args['EmployeeID']:
+            result.balance = args["EmployeeID"]
+        
+        if args['ExpenseDate']:
+            result.balance = args["ExpenseDate"]
+        
+        if args['Amount']:
+            result.balance = args["Amount"]
+        
+        if args['Purpose']:
+            result.balance = args["Purpose"]
+        
+        if args['DepartmentCode']:
+            result.balance = args["DepartmentCode"]
 
         db.session.commit()
 
@@ -80,11 +108,11 @@ def delete(id):
         args = put_args.parse_args()
 
         # Check if field already exists
-        result = BankModel.query.get(id)
+        result = Claims.query.get(id)
         if not result:
-            abort(409, message = "Bank account does not exist")
+            abort(409, message = "Claim does not exist")
         
         db.session.delete(result)
         db.session.commit()
-        return "Customer deleted", 201 # 201 means created, by default 201 is okay!
+        return "Claim deleted", 201 # 201 means created, by default 201 is okay!
     
