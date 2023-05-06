ebcd1ddf4472a297070419c485f2dea88ed03cd1
tjwjoe <tjwjoe1998@gmail.com>
2023-05-06 16:15:07 +0800

    feat(LoginPage): Store JWT token in LocalStorage

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,41 +1,55 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import { useNavigate } from 'react-router-dom';
-import { dummyUser } from "../data/users";
-import {Button, Checkbox} from 'antd';
+import {Button, Checkbox, Form, Input} from 'antd';
 import "./LoginPage.css";
 import DBSLogo from "../assets/cdnlogo.com_dbs-bank.svg";
 
 const LoginPage = () => {
-
-  const [employeeId, setEmployeeId] = useState("");
-  const [password, setPassword] = useState("");
+  const [employeeId, setEmployeeId] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
   const [isFailedLogin, setIsFailedLogin] = useState(false);
   const [isRememberMe, setIsRememberMe] = useState(false);
 
   const navigate = useNavigate();
 
-  const handleLogin = (): void => {
+  // If user has cookie, redirect them immediately to dashboard
+  useEffect(() => {
+    const existingToken = localStorage.getItem("dbsJWT");
+    if (existingToken) {
+      navigate("/ListClaims");
+    }
+  }, [])
+
+  const handleLogin = async (): Promise<void> => {
     // send login request to backend
     // if fail, display invalid login credentials message
     // if success, navigate to /ListClaims
-
-    // DUMMY VALIDATION
-    if (employeeId === dummyUser.EmployeeID) { 
-      console.log("Login Success!");
-      navigate("/ListClaims");
-    } else {
-      console.log("Login failed!!");
-      setIsFailedLogin(true);
+    try {
+      const bodyToSend = JSON.stringify({
+        ID: employeeId,
+        Password: password
+      });
+      console.log(bodyToSend);
+      await fetch("http://localhost:5000/", {
+        method: "POST",
+        mode: "cors",
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: bodyToSend
+      }).then((response) => response.json()).then((response) => {
+        console.log(response);
+        if (response.code == 200) {
+          localStorage.setItem("dbsJWT", response.token);
+          navigate("/ListClaims");
+        } else {
+          setIsFailedLogin(true);
+        }
+      })
+    } catch (err) {
+      console.log(err);
+      alert("An unexpected error occurred! Please try again.");
     }
-
-    /*
-    axios.get(...url, LoginDetails).then(() => {
-      
-    })
-    */
-
-
-    // navigate("/ListClaims");
   }
 
   const handleEmployeeIdInput = (event: React.FormEvent<HTMLInputElement>): void => {
@@ -60,21 +74,19 @@ const LoginPage = () => {
   }
 
   return (
-    <div className="LoginPage">
-      <div className="header">
+    <Form className="LoginPage">
+      <div className="LoginPageHeader">
         <img src={DBSLogo}/>
         <h1>Login To DBS Portal</h1>
       </div>
       <div className="InpuFieldsWrapper">
         <div className="InputFields">
-          <div>
-            <label>Employee Id:</label>
-            <input type="text" name="employeeId" placeholder="Employee ID" onChange={(event) => handleEmployeeIdInput(event)}></input>
-          </div>
-          <div>
-            <label>Password:</label>
-            <input type="password" name="password" placeholder="Password" onChange={(event) => handlePasswordInput(event)}></input>
-          </div>
+          <Form.Item label="Employee Id">
+            <Input onChange={(event) => handleEmployeeIdInput(event)}/>
+          </Form.Item>
+          <Form.Item label="Password">
+            <Input.Password onChange={(event) => handlePasswordInput(event)}/>
+          </Form.Item>
           <div>
             <Checkbox name="RememberMe" checked={isRememberMe} onChange={toggleRememberMe}/>
             <label htmlFor="RememberMe">Remember me</label>
@@ -84,7 +96,7 @@ const LoginPage = () => {
         </div>
       </div>
       
-    </div>
+    </Form>
   )
 }
 
