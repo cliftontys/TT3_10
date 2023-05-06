5ddc24566983865b41cec5f4d5486403cdbf4773
ch4ntel <88483937+ch4ntel@users.noreply.github.com>
2023-05-06 16:31:28 +0800

    authentication (backend) : update

--- Python/backend/controllers/claims.py
+++ Python/backend/controllers/claims.py
@@ -6,7 +6,7 @@ from flask_jwt_extended import create_access_token
 from flask_jwt_extended import JWTManager
 from flask_jwt_extended import jwt_required
 from flask_jwt_extended import get_jwt_identity
-
+import json
 
 # Making sure that the request parsed fits the below guidelines
 put_args = reqparse.RequestParser()
@@ -50,7 +50,7 @@ def get(id):
             return jsonify(
              {
                   "code": 404,
-                  "body": "Employee not found"
+                  "body": "Claim not found"
              }
             )
         return jsonify(
@@ -63,87 +63,74 @@ def get(id):
 # @marshal_with(resource_fields)
 @ jwt_required
 def put(id):
-    # Get arguments from put_args as defined above, where missing fields will have error
-    args = put_args.parse_args()
-    Client = Claims(ClaimID = args['ClaimID'], ProjectID = args['ProjectID'], CurrencyID = args['CurrencyID'],
-                        EmployeeID = args['EmployeeID'], ExpenseDate = args['ExpenseDate'],
-                        Amount = args['Amount'], Purpose = args['Purpose'], DepartmentCode = args['DepartmentCode'])
+
+        currUserId = get_jwt_identity()
+
+        if (id == currUserId):
+            abort(409, message = "Claim already exists")
+
+        # Get arguments from put_args as defined above, where missing fields will have error
+        args = put_args.parse_args()
+        Client = Claims(ClaimID = args['ClaimID'], ProjectID = args['ProjectID'], CurrencyID = args['CurrencyID'],
+            EmployeeID = args['EmployeeID'], ExpenseDate = args['ExpenseDate'],
+            Amount = args['Amount'], Purpose = args['Purpose'], DepartmentCode = args['DepartmentCode'])
     
-    # Check if field already exists
-    result = Claims.query.filter_by(ClaimID = args['ClaimID']).first()
-    if result:
-        abort(409, message = "Claim already exists")
 
-    # Add object to staging
-    db.session.add(Client)
-    # Commit permanently
-    db.session.commit()
+        # Add object to staging
+        db.session.add(Client)
+        # Commit permanently
+        db.session.commit()
 
-    return Client, 201 # 201 means created, by default 201 is okay!
+        return 201, Client
 
 @ jwt_required
 def patch(id):
+        currUserId = get_jwt_identity()
+
+        if (id != currUserId):
+            abort(404, message = "Cannot update a claim that does not exist")
+        
+        result = Claims.query.filter_by(ClaimID = id)
+
+        args = update_args.parse_args()
+
+        if args['ProjectID']:
+            result.projectid = args["ProjectID"]
+        
+        if args['CurrencyID']:
+            result.currencyid = args["CurrencyID"]
+        
+        if args['EmployeeID']:
+            result.employeeid = args["EmployeeID"]
+        
+        if args['ExpenseDate']:
+            result.expensedate = args["ExpenseDate"]
+        
+        if args['Amount']:
+            result.amount = args["Amount"]
+        
+        if args['Purpose']:
+            result.purpose = args["Purpose"]
+        
+        if args['DepartmentCode']:
+            result.deptcode = args["DepartmentCode"]
+
+        db.session.commit()
+
+        return 201, result
+
+@ jwt_required
+def delete(id):
         
         currUserId = get_jwt_identity()
 
         if (id != currUserId):
-            return jsonify(
-                {
-                    "code": 401,
-                    "message": "Unauthorized"
-                }
-        ), 500
-
-        args = update_args.parse_args()
-        result = Claims.query.filter_by(ClaimID = id).first()
-        if not result:
-            return jsonify(
-             {
-                  "code": 404,
-                  "body": "Employee not found"
-             }
-            )
-        
-        if args['ProjectID']:
-            result.name = args["ProjectID"]
-        
-        if args['CurrencyID']:
-            result.bank = args["CurrencyID"]
-        
-        if args['EmployeeID']:
-            result.balance = args["EmployeeID"]
-        
-        if args['ExpenseDate']:
-            result.balance = args["ExpenseDate"]
-        
-        if args['Amount']:
-            result.balance = args["Amount"]
-        
-        if args['Purpose']:
-            result.balance = args["Purpose"]
-        
-        if args['DepartmentCode']:
-            result.balance = args["DepartmentCode"]
-
-        db.session.commit()
-
-        return jsonify(
-             {
-                  "code": 201,
-                  "body": "Record updated"
-             }
-        )
-
-@ jwt_required
-def delete(id):
-        args = put_args.parse_args()
-
-        # Check if field already exists
-        result = Claims.query.get(id)
-        if not result:
             abort(409, message = "Claim does not exist")
-        
+
+        result = Claims.query.filter_by(ClaimID = id)
         db.session.delete(result)
         db.session.commit()
-        return "Claim deleted", 201 # 201 means created, by default 201 is okay!
-    
+
+        message = 'Claim deleted successfully'
+        
+        return 201, message
